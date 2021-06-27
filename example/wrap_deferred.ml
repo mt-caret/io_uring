39885ddf33862ccd1b7a0827d346ecc2fb7e7aee
Blake Loring <blake@parsed.uk>
2021-06-27 20:50:06 +0800

    Primitive scheduling is working

--- example/wrap_deferred.ml
+++ example/wrap_deferred.ml
@@ -1,5 +1,4 @@
 open Core
-open Async_kernel
 open Core.Unix
 
 module Ring = struct
@@ -13,6 +12,7 @@ module Ring = struct
 
   let wait t =
     printf "Submit %i\n" (Io_uring.submit t.ring);
+    Stdio.Out_channel.flush Stdio.Out_channel.stdout;
     Io_uring.wait t.ring ~timeout:`Never;
     printf "Done wait\n";
     Io_uring.iter_completions t.ring ~f:(fun ~user_data ~res ~flags ->
@@ -21,105 +21,122 @@ module Ring = struct
     Io_uring.clear_completions t.ring;
     ()
   ;;
+
+  let global = make ~ring_size:1024;
+
+end
+
+module Scheduler = struct
+
+  type t = {
+    ring: Ring.t;
+    worklist: (unit -> unit) Queue.t;
+  }
+
+  let create () = { ring = Ring.global; worklist = Queue.create () }
+  let schedule t work = Queue.enqueue t.worklist work
+
+  let poll t = Ring.wait t.ring
+  let do_work t = Queue.iter t.worklist ~f:(fun x -> printf "Executing a thing\n"; x ()); Queue.clear t.worklist
+  let rec block_forever t = do_work t; poll t; block_forever t
+
+  let global = create ()
+end
+
+module Deferred = struct
+
+  type 'a t = {
+    mutable then_ : ([`Ok of 'a | `Eof | `Error ] -> unit) option;
+  }
+
+  let create creater_fun = let new_t = { then_ = None; } in
+    Scheduler.schedule Scheduler.global (fun () ->
+      printf "Scheduled thing executed\n";
+      creater_fun (fun result -> (
+            match new_t.then_ with
+            | Some v -> v result
+            | None -> printf "Warning: Ignored Result\n"
+      ))
+    );
+    new_t
+
+  let bind t f = t.then_ <- Some f; t
+
 end
 
 module Socket = struct
 
   type t =
-    { ring : Ring.t
-    ; fd : File_descr.t
+    { fd : File_descr.t
     }
 
-  let make ring fd = { ring; fd = fd }
+  let make fd = { fd = fd }
 
   let accept t =
     printf "Accept\n";
-    Deferred.create (fun result ->
+    Deferred.create (fun result_fn ->
         ignore(Io_uring.prepare_accept
-          t.ring.ring
+          Ring.global.ring
           Io_uring.Sqe_flags.none
           t.fd
           (fun res _flags ->
+            printf "Accept callback\n";
             let accept_result = if res = -1 then `Eof else `Ok (File_descr.of_int res) in
-            Ivar.fill result accept_result;) : Io_uring.Queued_sockaddr.t option) (* TODO: Don't ignore *)
+            result_fn accept_result) : Io_uring.Queued_sockaddr.t option) (* TODO: Don't ignore *)
       )
   ;;
 
   let read t ~max_length =
     let buffer = Bigstring.create max_length in
-    Deferred.create (fun result ->
+    Deferred.create (fun result_fn ->
       ignore (Io_uring.prepare_recv
-      t.ring.ring
+      Ring.global.ring
       Io_uring.Sqe_flags.none
       t.fd
       buffer
       (fun res _flags -> (
         let recv_result = if res = -1 then `Error else if res = 0 then `Eof else `Ok (res, buffer) in
-        Ivar.fill result recv_result;
+        result_fn recv_result
       )) : bool) (* TODO: Don't ignore *)
     )
   ;;
 
   let write t ~buffer =
-    Deferred.create (fun result ->
+    Deferred.create (fun result_fn ->
       ignore (Io_uring.prepare_write
-      t.ring.ring
+      Ring.global.ring
       Io_uring.Sqe_flags.none
       t.fd
       buffer
       ~offset:0
       (fun res _flags -> (
         let write_result = if res = -1 then `Error else if res = 0 then `Eof else `Ok (res, buffer) in
-        Ivar.fill result write_result;
+        result_fn write_result;
       )) : bool) (* TODO: Don't ignore *)
     )
   ;;
 
 end
 
-let run ~queue_depth:_ ~port ~backlog ~max_message_len:_ =
+let sched = Scheduler.create ()
+
+let run ~port ~backlog =
   let sockfd = socket ~domain:PF_INET ~kind:SOCK_STREAM ~protocol:0 () in
   setsockopt sockfd SO_REUSEADDR true;
   let addr = ADDR_INET (Unix.Inet_addr.localhost, port) in
   bind sockfd ~addr;
   listen sockfd ~backlog;
   printf "Listening\n";
-  let ring = Ring.make ~ring_size:1024 in
-  printf "Created ring\n";
-  let server_socket = Socket.make ring sockfd in
+  let server_socket = Socket.make sockfd in
   printf "Created server socket\n";
   let rec accept_fn () =
-    let%bind _new_socket = Socket.accept server_socket in
-    accept_fn ()
+    printf "Scheduling an accept\n";
+    Deferred.bind (Socket.accept server_socket) (fun _new_socket -> printf "Got new socket\n"; ignore (accept_fn () : Unix.File_descr.t Deferred.t) );
   in
-  ignore (accept_fn () : 'a Deferred.t);
-  printf "Listening\n";
-  while true do
-    printf "Waiting\n";
-    Stdio.Out_channel.flush Stdio.Out_channel.stdout;
-    Ring.wait ring;
-  done
+  ignore (accept_fn () : Unix.File_descr.t Deferred.t);
+  Scheduler.block_forever Scheduler.global;
 ;;
 
 let () =
-  Command.run
-  @@ Command.basic ~summary:"echo server using io_uring"
-  @@ let%map_open.Command queue_depth =
-       flag
-         "queue-depth"
-         (optional_with_default 2048 int)
-         ~doc:"INT submission completion queue depth"
-     and port = flag "port" (required int) ~doc:" port to listen on"
-     and backlog =
-       flag
-         "backlog"
-         (optional_with_default 100 int)
-         ~doc:"INT size of backlog for listen()"
-     and max_message_len =
-       flag
-         "max-message-len"
-         (optional_with_default 4096 int)
-         ~doc:"INT maximum size of messages"
-     in
-     fun () -> run ~queue_depth ~port ~backlog ~max_message_len
-;;
+  let _deferred_scheduler = run ~port:8000 ~backlog:64 in
+  ()
